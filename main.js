360ba0ffba8756c68a3d10af0b938d0b45e6f723
Emanuel <lopez.emmanuelmatias@gmail.com>
2022-04-18 21:30:29 -0300

    Creacion archivo

---
+++ main.js
@@ -0,0 +1,9 @@
+
+// 
+const edadDelUsuario = parseInt(prompt("¿Cuál es tu edad?"));
+
+if(edadDelUsuario >= 18){
+    alert("Eres mayor de edad");
+}else{
+   alert("Eres menor de edad, no puedes ingresar.");
+}
